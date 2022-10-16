9c5122b70d2fcf8c93b303ae48b293e12f16b257
2205551030_Komang Ayu Agung Arya Aprilia <114905113+ayuaprliia@users.noreply.github.com>
2022-10-16 21:32:34 +0800

    Add files via upload

---
+++ fungsi kecepatan.c
@@ -0,0 +1,17 @@
+//fungsi rumus kecepatan, jarak, dan waktu
+
+#include <stdio.h>
+
+//deklarasi fungsi rumus kecepatan
+int kecepatan (int w, int j){
+	int kecepatan = jarak/waktu;
+	
+	return kecepatan;
+}
+
+int main (){
+
+int j, w, kecepatan;
+
+printf("Masukkan nilai waktu ")
+}
